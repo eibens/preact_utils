3918c6a2603251e916ce5c48728987afdaf5880f
eibens <l.eibensteiner@gmail.com>
2021-10-27 01:44:52 +0200

    chore: upgrade build and CI

--- dev.ts
+++ dev.ts
@@ -1,7 +1,9 @@
-import * as edcb from "https://deno.land/x/edcb@v0.7.1/cli.ts";
+import { cli } from "https://deno.land/x/edcb@v0.9.0-alpha.2/cli.ts";
 
 if (import.meta.main) {
-  await edcb.build({
-    ignore: "deps",
+  await cli({
+    check: {
+      ignore: "deps",
+    },
   });
 }
