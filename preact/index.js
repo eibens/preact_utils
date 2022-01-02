c41d9cef5de2cf4d138053ba856a03925fd739c7
eibens <l.eibensteiner@gmail.com>
2022-01-02 16:39:07 +0100

    wip: add a deno port of preact
    - Add the src and hook/src files.
    - Add .js and .ts extensions to all imports.

---
+++ preact/index.js
@@ -0,0 +1,13 @@
+export { hydrate, render } from "./render.js";
+export {
+  createElement,
+  createElement as h,
+  createRef,
+  Fragment,
+  isValidElement,
+} from "./create-element.js";
+export { Component } from "./component.js";
+export { cloneElement } from "./clone-element.js";
+export { createContext } from "./create-context.js";
+export { toChildArray } from "./diff/children.js";
+export { default as options } from "./options.js";
